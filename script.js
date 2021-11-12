677295e9e257e45c2f86421f713c4a3dbe1e8cc1
Garrett Broadhead <gwbroadhead@gmail.com>
2021-11-11 23:21:08 -0800

    Completed password generator. Currently testing

--- script.js
+++ script.js
@@ -1,81 +1,107 @@
+const lowerList = 'abcdefghijklmnopqrstuvwxyz';
+const numList = '1234567890';
+const specialList = `!@#$%^&*()[]{};':",.<>/?`;
 
-//.toUppercase() USE FOR UPPERCASES
+var upperAlpha =[];
 
-const alphaList = 'abcdefghijklmnopqrstuvwxyz' 
-const numList = '1234567890'
-const specialList = '!@#$%^&*()'
-let capital = true
-let lower = true
-let number = true
-let special = true
+let userChoices = "";
+
+function toUpper(x) {
+  return x.toUpperCase();
+}
+upperList = lowerList.toUpperCase()
 
 
 document.getElementById('generate').addEventListener('click', () => {
+  let confirmLength = prompt('Enter the desired password length. Minimum of 8 and maximum of 128');
 
-  let passLength = prompt('Enter the desired password length. Minimum of 8 and maximum of 128');
-  
-  while (passLength < 8 || passLength > 128) {
-  passLength = prompt('Length needs to be a minimum of 8 and a maximum of 128');
-    if (passLength >= 8 && passLength <= 128) {
-      confirm(`You chose ${passLength}`)
+  while (confirmLength < 8 || confirmLength > 128 || isNaN(confirmLength)) {
+    confirmLength = prompt('Length needs to be a minimum of 8 and a maximum of 128');
+    if (confirmLength >= 8 && confirmLength <= 128) {
     }
-  } 
-
-  let capital = confirm('Do you want capital letters? Yes = Ok No = Cancel');
-  if (capital === true ) {
-    confirm("You clicked Ok.")
-    let capital = alphaList.toUpperCase()
-  } else {
-    confirm('You clicked Cancel.')
   }
 
-  let lower = confirm('Do you want lower case letters? Yes = Ok No = Cancel');
-  if (lower === true) {
-    confirm("You clicked Ok.")
-    let lower = alphaList
-  } else {
-    confirm('You clicked Cancel.')
+  let confirmUpper = confirm('Do you want capital letters? Yes = Ok No = Cancel');
+
+  let confirmLower = confirm('Do you want lower case letters? Yes = Ok No = Cancel');
+
+  let confirmNum = confirm('Do you want numbers? Yes = Ok No = Cancel');
+
+  let confirmSpecial = confirm('Do you want special characters? Yes = Ok No = Cancel');
+
+  if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpecial) {
+    userChoices = confirm("You must select a criteria!");
+  }
+  else if (confirmUpper && confirmLower && confirmNum && confirmSpecial) {
+    userChoices = lowerList.concat(upperList, specialList, numList);
+  }
+  else if (confirmUpper && confirmNum && confirmSpecial) {
+    userChoices = specialList.concat(upperList, numList);
+  }
+  else if (confirmLower && confirmNum && confirmSpecial) {
+    userChoices = specialList.concat(lowerList, numList);
+  }
+  else if (confirmUpper && confirmLower && confirmSpecial) {
+    userChoices = specialList.concat(upperList, lowerList);
+  }
+  else if (confirmUpper && confirmLower && confirmNum) {
+    userChoices = lowerList.concat(upperList, numList);
+  }
+  else if (confirmNum && confirmSpecial) {
+    userChoices = specialList.concat(numList);
+  }
+  else if (confirmLower && confirmSpecial) {
+    userChoices = specialList.concat(lowerList);
+  }
+  else if (confirmUpper && confirmSpecial) {
+    userChoices = specialList.concat(upperList);
+  }
+  else if (confirmLower && confirmNum) {
+    userChoices = lowerList.concat(numList);
+  }
+  else if (confirmUpper && confirmLower) {
+    userChoices = lowerList.concat(upperList);
+  }
+  else if (confirmUpper && confirmNum) {
+    userChoices = numList.concat(upperList);
+  }
+  else if (confirmSpecial) {
+    userChoices = specialList;
+  }
+  else if (confirmNum) {
+    userChoices = numList;
+  }
+  else if (confirmLower) {
+    userChoices = lowerList;
+  }
+  else if (confirmUpper) {
+    userChoices = upperAlpha.concat(upperList);
+  };
+
+
+  var password = [];
+
+
+  for (i = 0; i < confirmLength; i++) {
+    choices = userChoices[Math.floor(Math.random() * userChoices.length)];
+    password.push(choices);
   }
 
-  let number = confirm('Do you want numbers? Yes = Ok No = Cancel');
-  if (number === true) {
-    confirm("You clicked Ok.")
-    let number = numList
-  } else {
-    confirm('You clicked Cancel.')
-  }
-
-  let special = confirm('Do you want special characters? Yes = Ok No = Cancel');
-  if (special === true) {
-    confirm("You clicked Ok.")
-    let special = specialList
-  } else {
-    confirm('You clicked Cancel.')
-  }
-
+  var ps = password.join("");
+  UserInput(ps);
+  return ps;
+  
 
 })
 
-function generateNewPassword() {
-  let password = "";
 
-  var allowed = {};
-  if (uppers) password += rando(allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
-  if (lowers) password += rando(allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
-  if (numbers) password += rando(allowed.numbers = "1234567890");
-  if (symbols) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
 
-  for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
-
-  document.getElementById("password").value = randoSequence(password).join("");
+function UserInput(ps) {
+  document.getElementById("password").textContent = ps;
 }
 
-
-// let needPass = document.getElementById("password").innerHTML = password;
-
-
-  
-
-
-
+document.getElementById("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
 
